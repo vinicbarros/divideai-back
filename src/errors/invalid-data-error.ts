6a495a4c83bc96d2b1563812bb5b2125661dd3d0
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-01-23 10:12:58 -0300

    feat: created middleware section and body validation

---
+++ src/errors/invalid-data-error.ts
@@ -0,0 +1,15 @@
+import { ApplicationError } from "@/protocols";
+
+export function invalidDataError(
+  details: string[]
+): ApplicationInvalidDataError {
+  return {
+    name: "InvalidDataError",
+    message: "Invalid data",
+    details,
+  };
+}
+
+type ApplicationInvalidDataError = ApplicationError & {
+  details: string[];
+};
