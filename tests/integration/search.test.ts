a5f5d8269cd2c482e50f0f28526c92794c47a676
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-03 10:29:32 -0300

    test: added new search test coverage

--- tests/integration/search.test.ts
+++ tests/integration/search.test.ts
@@ -2,7 +2,7 @@ import app, { close, init } from "@/app";
 import httpStatus from "http-status";
 import supertest from "supertest";
 import * as jwt from "jsonwebtoken";
-import { createUser } from "../factories";
+import { createAcceptedFriendRequest, createUser } from "../factories";
 import { cleanDb, generateValidToken } from "../helpers";
 import { faker } from "@faker-js/faker";
 
@@ -21,7 +21,7 @@ afterAll(async () => {
 
 const server = supertest(app);
 
-describe("GET /search", () => {
+describe("GET /search/:email", () => {
   it("should respond with status 401 if no token is no given", async () => {
     const response = await server.get("/search");
 
@@ -49,26 +49,13 @@ describe("GET /search", () => {
   });
 
   describe("when token is valid", () => {
-    it("should respond with status 400 when body is not valid", async () => {
-      const token = await generateValidToken();
-      const invalidBody = { [faker.lorem.word()]: faker.lorem.word() };
-
-      const response = await server
-        .post("/bill")
-        .set("Authorization", `Bearer ${token}`)
-        .send(invalidBody);
-
-      expect(response.status).toBe(httpStatus.BAD_REQUEST);
-    });
     describe("when body is valid", () => {
       it("should respond with status 200 and an empty array result", async () => {
         const token = await generateValidToken();
-        const validBody = { email: faker.lorem.word() };
 
         const response = await server
-          .get("/search")
-          .set("Authorization", `Bearer ${token}`)
-          .send(validBody);
+          .get(`/search/${faker.lorem.word()}`)
+          .set("Authorization", `Bearer ${token}`);
 
         expect(response.status).toBe(httpStatus.OK);
       });
@@ -76,12 +63,10 @@ describe("GET /search", () => {
       it("should respond with status 200 and result search", async () => {
         const token = await generateValidToken();
         const user = await createUser();
-        const validBody = { email: user.email };
 
         const response = await server
-          .get("/search")
-          .set("Authorization", `Bearer ${token}`)
-          .send(validBody);
+          .get(`/search/${user.email}`)
+          .set("Authorization", `Bearer ${token}`);
 
         expect(response.status).toBe(httpStatus.OK);
         expect(response.body).toEqual([
@@ -89,6 +74,76 @@ describe("GET /search", () => {
             id: user.id,
             name: user.name,
             email: user.email,
+          },
+        ]);
+      });
+    });
+  });
+});
+
+describe("GET /search/friends/:email", () => {
+  it("should respond with status 401 if no token is no given", async () => {
+    const response = await server.get("/search/friend");
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+
+    const response = await server
+      .get("/search/friend")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign(
+      { userId: userWithoutSession.id },
+      process.env.JWT_SECRET as string
+    );
+
+    const response = await server
+      .get("/search/friend")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    describe("when body is valid", () => {
+      it("should respond with status 200 and an empty array result", async () => {
+        const token = await generateValidToken();
+
+        const response = await server
+          .get(`/search/friends/${faker.lorem.word()}`)
+          .set("Authorization", `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.OK);
+      });
+
+      it("should respond with status 200 and result search", async () => {
+        const user = await createUser();
+        const token = await generateValidToken(user);
+        const friend = await createUser();
+
+        const friendRequest = await createAcceptedFriendRequest({
+          userId: user.id,
+          friendId: friend.id,
+        });
+        const response = await server
+          .get(`/search/friends/${friend.email}`)
+          .set("Authorization", `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.OK);
+        expect(response.body).toEqual([
+          {
+            id: friend.id,
+            friendRequestId: friendRequest.id,
+            name: friend.name,
+            email: friend.email,
           },
         ]);
       });
