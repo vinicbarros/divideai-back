105707b5adfe03180002361375745db72bce027f
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-03 23:13:06 -0300

    chore: create prisma seed for category

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const data = [
+  { name: "Viagem" },
+  { name: "Casa" },
+  { name: "Evento" },
+  { name: "Projeto" },
+  { name: "Investimento" },
+  { name: "Churrasco" },
+  { name: "Rolê" },
+];
+
+async function main() {
+  let categories = await prisma.category.findMany();
+  if (categories.length === 0) {
+    await prisma.category.createMany({
+      data,
+    });
+  }
+
+  console.log({ data });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
